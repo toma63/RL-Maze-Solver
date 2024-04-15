28de684a94fcb80cb93e3b9473a34bd4e7197fb1
Tom Arns <tomrarns@gmail.com>
2024-04-15 14:25:41 -0700

    Add MazeCell, shuffle, randomStep, encllosed, step, change display to makeDisplayGrid, makeMaze works

--- js/index.js
+++ js/index.js
@@ -1,10 +1,24 @@
+function shuffle(array) {
+    let currentIndex = array.length;
+    while (currentIndex !== 0) {
+        let randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+        [array[currentIndex], array[randomIndex]] = [array[randomIndex], array[currentIndex]];
+    }
+    return array;
+}
+
 class Maze {
     constructor(rows = 10, cols = 10) {
         this.rows = rows;
         this.cols = cols;
+        // initialize occupancy matrix
+        // 0 for empty, MazeCell if occupied
+        this.cellMatrix = Array.from({ length: this.rows }, 
+            () => Array(this.cols).fill(0));
     }
 
-    display(gridSize = 100) {
+    makeDisplayGrid(gridSize = 100) {
         this.canvas = document.getElementById('maze-canvas');
         this.canvas.width = this.cols * gridSize;
         this.canvas.height = this.rows * gridSize;
@@ -29,9 +43,64 @@ class Maze {
         this.ctx.strokeStyle = "#ddd";  // Grid default line color
         this.ctx.stroke();
     }
+
+    placeCell(mazeCell) {
+        this.cellMatrix[mazeCell.y][mazeCell.x] = mazeCell;
+        return mazeCell;
+    }
+
+    makeMaze(start = new MazeCell(0, 0, this)) {
+        let currentCell = start;
+        do {
+            this.placeCell(currentCell);
+            currentCell = currentCell.randomStep();
+            console.log(currentCell);
+        } while (currentCell);
+    }
 }
 
+class MazeCell {
+
+    constructor(x, y, maze) {
+        this.x = x;
+        this.y = y;
+        this.maze = maze;
+    }
+
+    static moves = [{ x: 0, y: 1 }, { x: 1, y: 0 }, { x: 0, y: -1 }, { x: -1, y: 0 }];
+
+    step(move) {
+        return new MazeCell(this.x + move.x, this.y + move.y, this.maze);
+    }
+
+    enclosed() {
+        return ((this.x > 0 && this.maze.cellMatrix[this.y][this.x - 1]) &&
+                (this.x < this.maze.cols - 1 && this.maze.cellMatrix[this.y][this.x + 1]) &&
+                (this.y > 0 && this.maze.cellMatrix[this.y - 1][this.x]) &&
+                (this.y < this.maze.rows - 1 && this.maze.cellMatrix[this.y + 1][this.x])) ;
+    }
+
+    // move at random to an adjacent cell
+    // return false if no moves are possible
+    randomStep() {
+        if (this.enclosed()) {
+            return false;
+        }
+        let newCell = new MazeCell(0, 0, this.maze)
+        do {
+            newCell = this.step(shuffle(MazeCell.moves)[0]);
+        // keep going while illegal
+        } while (newCell.x < 0 ||
+                 newCell.y < 0 || 
+                 newCell.x >= this.maze.cols || 
+                 newCell.y >= this.maze.rows ||
+                 this.maze.cellMatrix[newCell.y][newCell.x] !== 0);
+        return newCell;
+    }
+}
+
+
 mz = new Maze();
-mz.display();
+mz.makeDisplayGrid();
 
 
